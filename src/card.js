f6a149d59805b8166a528763a00813ff07f6f338
Cova97 <acovamartinez@gmail.com>
2023-04-25 16:17:48 -0600

    cards

--- src/card.js
+++ src/card.js
@@ -3,10 +3,12 @@ import React from "react";
 const Card = (props) => {
     return(
         <>
-            <div>
-                <h2>{props.nombre} {props.apellido}</h2>
-                <p>Edad: {props.edad}</p>
-                <button onClick={props.onEliminar}>Eliminar</button>
+            <div class="card">
+                <div class="container">
+                    <h2>{props.nombre} {props.apellido}</h2>
+                    <p>Edad: {props.edad}</p>
+                    <button onClick={props.onEliminar}>Eliminar</button>
+                </div>
             </div>
         </>
     );
